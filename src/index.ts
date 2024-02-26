478ec4b9a866727cb5dba6d6e5d459c188e27cd0
Арсен <arsendag505@gmail.com>
2024-02-26 22:04:12 +0300

    add Typescript to app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const readline = require('readline');
-const { PassThrough } = require('stream');
+import * as fs from 'fs'
+import * as readline from 'readline'
+import { PassThrough } from 'stream'
 
-async function sortLargeFile(filePath, chunkSize) {
+async function sortLargeFile(filePath: string, chunkSize: number) {
     let chunkCount = 0;
     let chunks = [];
     let currentChunk = [];
@@ -30,7 +30,7 @@ async function sortLargeFile(filePath, chunkSize) {
 
     const outputStream = fs.createWriteStream('sorted_file.txt');
     const streams = chunks.map(chunkFile => fs.createReadStream(chunkFile));
-    let mergedStream = PassThrough();
+    let mergedStream = new PassThrough();
     streams.forEach(stream => {
         stream.pipe(mergedStream, { end: false });
         stream.on('end', () => {
@@ -40,10 +40,9 @@ async function sortLargeFile(filePath, chunkSize) {
     mergedStream.pipe(outputStream);
 }
 
-// Используем
 const filePath = '160-KB.txt';
 const chunkSize = 500000;
 
-sortLargeFile(filePath, chunkSize)
-    .then(() => console.log('Файл отсортирован'))
-    .catch(error => console.error('Ошибка сортировки:', error));
+// sortLargeFile(filePath, chunkSize)
+//     .then(() => console.log('Файл отсортирован'))
+//     .catch(error => console.error('Ошибка сортировки:', error));
